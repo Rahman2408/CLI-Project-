3c7b4788659992dc2a63e989b702e67426792417
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-02-03 14:47:38 -0500

    updates api

--- lib/UI.rb
+++ lib/UI.rb
@@ -1,14 +1,33 @@
 require_relative '../config/environment'
 
 class UI
-
+@@start_point
+@@end_point
     def greeting
     
         puts  "Hi!" 
         sleep(1)
-        puts "Welcome to my translator!"
+        puts "Welcome to the map!"
     end
 
+    def user_from
+        puts "start point?"
+        input = gets.strip 
+        @@start_point = input
+    end
 
+    def user_to
+        puts "end point?"
+        input2 = gets.strip
+        @@end_point = input2
+    end
+ 
+   def self.start
+    @@start_point
+   end
+   
+   def self.end
+    @@end_point
+   end
 
 end
--- lib/api.rb
+++ lib/api.rb
@@ -1,8 +1,16 @@
 require_relative '../config/environment'
 
 
+
 class API
+  @@api_key = "fdff312267msh8e7d79a718d7a7fp1027f1jsn5b63bcb0b23c"
+  @@url = "https://montanaflynn-mapit.p.rapidapi.com/directions?ending=#{end_point}&starting=#{start_point}"
+start_point = UI.start_point
+end_point = UI.end_point
 
+  def self.request_api
+    uri = URI.parse(@@url)
+    request = Net::HTTP.get_response(uri)
+    
+  end
 
-
-end
